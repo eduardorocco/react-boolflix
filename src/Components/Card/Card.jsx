84da231220610661703a2fc882c1cd7f2538103e
eduardorocco <eduardo_rocco@hotmail.it>
2024-12-12 15:28:59 +0100

    ADD flags to card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,41 @@
+import de from '../../assets/flags/de.png'
+import en from '../../assets/flags/en.png'
+import es from '../../assets/flags/es.png'
+import fr from '../../assets/flags/fr.png'
+import it from '../../assets/flags/it.png'
+
 export default function Card({ obj }) {
 
     const { title, original_title, original_language, vote_average } = obj
+
+    const supportedLenguages = ['de', 'en', 'es', 'fr', 'it']
+
+    const getFlag = (leng) => {
+        switch (leng) {
+            case 'de':
+                return de
+            case 'en':
+                return en
+            case 'es':
+                return es
+            case 'fr':
+                return fr
+            case 'it':
+                return it
+            default:
+                return leng
+        }
+    }
     return (
         <>
 
             <ul>
                 <li>{title}</li>
                 <li>{original_title}</li>
-                <li>{original_language}</li>
+                <li>
+                    {supportedLenguages.includes(original_language) ? (
+                        <img src={getFlag(original_language)} alt="" />) : <div>{original_language}</div>}
+                </li>
                 <li>{vote_average}</li>
             </ul>
 
