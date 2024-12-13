3b75d3dabf33fbcb6c1e359fc237566cca4b1560
eduardorocco <eduardo_rocco@hotmail.it>
2024-12-13 10:39:23 +0100

    ADD card structure

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,10 +3,18 @@ import en from '../../assets/flags/en.png'
 import es from '../../assets/flags/es.png'
 import fr from '../../assets/flags/fr.png'
 import it from '../../assets/flags/it.png'
+import style from './Card.module.css'
 
 export default function Card({ obj }) {
 
-    const { name, original_name, title, original_title, original_language, vote_average } = obj
+    const {
+        name,
+        original_name,
+        title, original_title,
+        original_language,
+        vote_average,
+        poster_path
+    } = obj
 
     const supportedLenguages = ['de', 'en', 'es', 'fr', 'it']
 
@@ -27,9 +35,19 @@ export default function Card({ obj }) {
         }
     }
     return (
-        <>
+        <div className="card">
+            <div className="card-title">
+                <p></p>
+                <p></p>
+            </div>
+            <div className="card-body">
 
-            <ul>
+            </div>
+        </div>
+    )
+}
+
+           {/* <ul>
                 <li>{title ? title : name}</li>
                 <li>{original_title ? original_title : original_name}</li>
                 <li>
@@ -37,9 +55,4 @@ export default function Card({ obj }) {
                         <img src={getFlag(original_language)} alt="" />) : <div>{original_language}</div>}
                 </li>
                 <li>{vote_average}</li>
-            </ul>
-
-        </>
-
-    )
-}
+            </ul> */}
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,20 +1,23 @@
 import Card from "../Card/Card"
 import GlobalContext from "../../Context/GlobalContext"
 import { useContext } from "react"
+import style from './Main.module.css'
 
 export default function Main() {
 
     const { search } = useContext(GlobalContext)
 
     return (
-        <>
-            {search.map((movie) => (
-                <Card
-                    key={movie.id}
-                    obj={movie}
-                />
-            ))}
-        </>
-
+        <div className={style.container}>
+            <div className={style.row}>
+                {search.map((movie) => (
+                    <div key={movie.id} className={style.col_2}>
+                        <Card
+                            obj={movie}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
